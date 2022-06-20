57e937b445c41016764b6dc73e1a23d65f38980a
Jared Mates <jaredmates45@gmail.com>
2022-06-20 16:09:02 -0700

    set up server in config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    // compress: true,
+    // historyApiFallback: true,
+  },
 };
+
+// ------------------------------
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const BundleAnalyzerPlugin =
+//   require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+// mode: 'development',
+// entry: {
+//   bundle: path.resolve(__dirname, 'src/index.js'),
+// },
+// output: {
+//   path: path.resolve(__dirname, 'dist'),
+//   filename: '[name][contenthash].js',
+//   clean: true,
+//   assetModuleFilename: '[name][ext]',
+// },
